253abbbd48dc7df41086ecfec6890456678f2013
Onyinye-Judith <onyinyejudith55@yahoo.com>
2024-01-09 17:57:36 -0800

    python-data_structure

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,3 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return Nonone
     return my_list[idx]
-
-# Example usage
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-result = element_at(my_list, idx)
-print("Element at index {:d} is {}".format(idx, result))
