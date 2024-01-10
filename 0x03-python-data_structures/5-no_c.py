3f1e7694884ffb743927bab4006b253b3e37bfb1
Onyinye-Judith <onyinyejudith55@yahoo.com>
2024-01-09 18:36:57 -0800

    python-data_structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = ""
-    for elements in my_string:
-        if elements != "c" and elements != "C":
-            new_string += elements
-            return new_string
+    filtered_string = ''.join(cha for char in my_string if char.lower() != 'c')
+    return filtered_string
