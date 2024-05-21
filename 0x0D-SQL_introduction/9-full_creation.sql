6b1794dd2cd868e7e87905a948ea459dc7bedc56
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-05-21 21:46:57 +0100

    My-SQL_Databases

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS new_table (
+	id INT,
+	username VARCHAR(256),
+	points INT
+	);
+
+	INSERT INTO new_table (id, username, points)
+	VALUES
+	(1, "User1", 15),
+	(2, "User2", 7),
+	(3, "User3", 20),
+	(4, "User4", 12);CREATE TABLE IF NOT EXISTS new_table (
+		id INT,
+	
