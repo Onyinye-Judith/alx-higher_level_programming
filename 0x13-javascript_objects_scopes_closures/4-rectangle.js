30c3326a6a03015657974e9cbebed9c2bfe7aabd
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-06-20 15:22:00 +0100

    Java-Script

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
+
 class Rectangle {
 	  constructor(w, h) {
-		      if (typeof w !== 'number' || typeof h !== 'number') {
-			            return {};
+		      if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+			            this.width = w;
+			            this.height = h;
 			          }
-		      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-			            return {};
-			          }
-		      this.width = w;
-		      this.height = h;
 		    }
 
 	  print() {
-		      for (let i = 0; i < this.height; i++) {
-			            for (let j = 0; j < this.width; j++) {
-					            process.stdout.write('X ');
+		      if (this.width && this.height) {
+			            for (let i = 0; i < this.height; i++) {
+					            console.log('X'.repeat(this.width));
 					          }
-			            console.log();
 			          }
 		    }
 
 	  rotate() {
-		      [this.width, this.height] = [this.height, this.width];
+		      if (this.width && this.height) {
+			            [this.width, this.height] = [this.height, this.width];
+			          }
 		    }
 
 	  double() {
-		      this.width *= 2;
-		      this.height *= 2;
+		      if (this.width && this.height) {
+			            this.width *= 2;
+			            this.height *= 2;
+			          }
 		    }
 }
-export default Rectangle;
