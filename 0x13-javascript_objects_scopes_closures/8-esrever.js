9885cc2e1ff6e7d97acb8404faadf4b4c608e085
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-06-20 15:48:36 +0100

    Java-Script

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,5 @@
-exports.esrever = function (list) {
+#!/usr/bin/node
+eports.esrever = function (list) {
 	  let reversedList = [];
 	  for (let i = list.length - 1; i >= 0; i--) {
 		      reversedList.push(list[i]);
