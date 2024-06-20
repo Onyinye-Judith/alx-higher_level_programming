443d4008d85bbb14e618aaf2be3c90de982d24f8
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-06-20 14:35:31 +0100

    Java-Script

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,13 @@
+#!/usr/bin/node
 class Rectangle {
 	constructor(w, h) {
 		if (typeof w !== 'number' || typeof h !== 'number') {
-			return {};
-		}
-		if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-			return {};
-		}
-		this.width = w;
-		this.height = h;
-	}
+			      return {};
+			    }
+		    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+			          return {};
+			        }
+		    this.width = w;
+		    this.height = h;
+		  }
 }
